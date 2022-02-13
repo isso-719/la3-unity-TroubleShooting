0793626df68ddd8ee84bcaf133bddf1154372ed0
isso-719 <i@kazukiisogai.net>
2022-02-13 23:56:02 +0900

    first commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.forward * 12f + Vector3.up * 8f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+		if (timer > 10f)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Target"){
+			other.gameObject.GetComponent<MeshRenderer>().material.color=Color.blue;
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+	Rigidbody rigidbody;
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Instantiate(bullet, transform.position, transform.rotation);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(Vector3.up * 40f * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.up * -40f * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+		{
+			rigidbody.velocity = transform.forward * 5f;
+		}
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+		{
+			rigidbody.velocity = transform.forward * -5f;
+		}
+	}
+
+}
